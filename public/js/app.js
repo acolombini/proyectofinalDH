bee4af79e2b6d868ad547a7ac442483ce6e3c043
jsp9892@gmail.com <jsp9892@gmail.com>
2020-03-09 01:21:25 -0300

    Configuro el view layout, traigo los resources del sprint de php y agrego paquete de auth

--- public/js/app.js
+++ public/js/app.js
@@ -37119,8 +37119,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\ProyectoFinalDH-Laravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\ProyectoFinalDH-Laravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\proyectofinalDH\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\proyectofinalDH\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    @include('includes/head')
+</head>
+
+<body>
+    <div id="app">
+        @include('includes/header')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+        @include('includes/footer')
+    </div>
+</body>
+
+</html>
