14f327724fa93765bbf9d28e577966a5eea72c15
Ignacio <mfignacio97@hotmail.com>
2020-05-15 10:38:56 -0300

    Agrego categorías y paginacion a la página de todos los productos

--- resources/views/front/productos/indexAll.blade.php
+++ resources/views/front/productos/indexAll.blade.php
@@ -4,9 +4,23 @@
 <main>
     <div class="container">
     <div class="card-container">
-        <div class="row justify-content-around">
+        <h2 class="text-center py-3">Nuestros Productos</h2>
+            <h4>Categorías</h4>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                @forelse ($categorias as $categoria)
+                    <li class="breadcrumb-item active" aria-current="page"><a href="categoria/{{$categoria->id}}">{{$categoria->nombre_categoria}}</a></li>
+                @empty
+                    No hay categorías.
+                @endforelse
+                </ol>
+            </nav>
+            <div class="justify-content-center pagination pagination-lg mt-3">
+                {{$productos->links()}}
+            </div>
+            
+            <div class="row justify-content-around">
             @forelse ($productos as $producto)
-
             <div class="product-card my-2 col-6 col-md-4 col-lg-3 ">
                 <div class="product-header">
                     <img src="{{$producto->poster ? '/storage/product_poster/'.$producto->poster : asset('img/defaultgamecardimage.png') }}" alt="imagen del producto">
@@ -45,8 +59,8 @@
         </div>
     </div>
 
-        <div class="row">
-            {{-- {{$productos->links()}} --}}
+        <div class="justify-content-center pagination pagination-lg mt-3">
+            {{$productos->links()}}
         </div>
 
     </div>
